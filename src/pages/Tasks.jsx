fb0c72c0d44f272d13003f232eca616ba3cca7ff
Yomi <123415710+Yommi@users.noreply.github.com>
2025-09-28 01:00:12 -0400

    add task functionality to frontend

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -1,22 +1,35 @@
-import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 import { FaRegCircleDot } from "react-icons/fa6";
 import { IoIosAddCircle } from "react-icons/io";
 import { MdDeleteForever } from "react-icons/md";
-import axios from "axios";
+import { ClipLoader } from "react-spinners";
+import { RiCloseLine } from "react-icons/ri";
 
 const apiUrl = import.meta.env.VITE_API_URL;
 
+const addSchema = Yup.object({
+  title: Yup.string()
+    .max(50, "Minmum of 50 characters")
+    .required("A task must have a title"),
+  description: Yup.string().required("A task must have a description"),
+});
 const Tasks = () => {
+  const [tasksLoading, setTasksLoading] = useState(true);
   const [tasks, setTasks] = useState([]);
   const [selectedTasks, setSelectedTasks] = useState([]);
-  const [openDelete, setOpenDelete] = useState(false);
+  const [openDeleteModal, setOpenDeleteModal] = useState(false);
+  const [openAddModal, setOpenAddModal] = useState(false);
+  const [deleteLoading, setDeleteLoading] = useState(false);
 
   useEffect(() => {
     console.log(selectedTasks);
   }, [selectedTasks]);
   useEffect(() => {
-    console.log(openDelete);
-  }, [openDelete]);
+    console.log(openDeleteModal);
+  }, [openDeleteModal]);
 
   useEffect(() => {
     fetchTasks();
@@ -25,6 +38,7 @@ const Tasks = () => {
   const fetchTasks = async () => {
     const token = localStorage.getItem("token");
     try {
+      setTasksLoading(true);
       const response = await axios.get(`${apiUrl}/tasks/me`, {
         headers: {
           Authorization: `Bearer ${token}`,
@@ -33,6 +47,8 @@ const Tasks = () => {
       setTasks(response.data.data);
     } catch (err) {
       console.log(err.message, err.stack);
+    } finally {
+      setTasksLoading(false);
     }
   };
 
@@ -55,9 +71,58 @@ const Tasks = () => {
 
   const handleDeleteIcon = () => {
     if (selectedTasks.length > 0) {
-      setOpenDelete(true);
+      setOpenDeleteModal(true);
     } else {
-      setOpenDelete(false);
+      setOpenDeleteModal(false);
+    }
+  };
+
+  const handleDeletion = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      console.log("token", token);
+      setDeleteLoading(true);
+      const response = await axios.delete(`${apiUrl}/tasks/`, {
+        data: { taskIds: selectedTasks },
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      fetchTasks();
+      setOpenDeleteModal(false);
+      setSelectedTasks([]);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setDeleteLoading(false);
+    }
+  };
+
+  const initialValues = {
+    title: "",
+    description: "",
+    date: new Date().toISOString().slice(0, 10),
+    status: "",
+    priority: "",
+  };
+
+  const handleAddSubmit = async (values, { resetForm, setSubmitting }) => {
+    try {
+      const token = localStorage.getItem("token");
+      const payload = {
+        ...values,
+        status: values.status === "completed",
+      };
+      await axios.post(`${apiUrl}/tasks`, payload, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      resetForm();
+      setOpenAddModal(false);
+      fetchTasks();
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
     }
   };
 
@@ -80,6 +145,13 @@ const Tasks = () => {
           />
         )}
       </div>
+
+      {tasksLoading && (
+        <div class="fixed inset-0 flex items-center justify-center z-50">
+          <div class="w-8 h-8 border-4 border-purple-500 border-t-transparent rounded-full animate-spin"></div>
+        </div>
+      )}
+
       <div className={tailwind.tasksArea}>
         {tasks?.map((task, i) => (
           <div className={tailwind.taskCont} key={i}>
@@ -106,10 +178,15 @@ const Tasks = () => {
           </div>
         ))}
         <div className="fixed bottom-10 right-10">
-          <IoIosAddCircle className={tailwind.buttons} size={45} />
+          <IoIosAddCircle
+            className={tailwind.buttons}
+            size={45}
+            onClick={() => setOpenAddModal(true)}
+          />
         </div>
         {/* Modal Window */}
-        {openDelete && (
+        {/* Delete Modal */}
+        {openDeleteModal && (
           <div className="fixed inset-0 bg-black/40 backdrop-blur-sm flex items-center justify-center z-50">
             <div className="bg-purple-500 rounded-2xl shadow-xl p-6 w-[40%] text-center">
               <h2 className="text-2xl font-semibold mb-4 text-white">Delete</h2>
@@ -117,18 +194,125 @@ const Tasks = () => {
                 Are you sure you want to delete the selected items?
               </p>
               <div className="flex justify-between w-[40%] mx-auto">
-                <button className="bg-green-500 text-black font-semibold px-6 py-1 rounded-lg hover:scale-105 hover:cursor-pointer">
-                  Yes
+                <button
+                  onClick={handleDeletion}
+                  className="bg-green-500 text-white font-semibold px-6 py-1 rounded-lg hover:scale-105 hover:cursor-pointer flex items-center"
+                >
+                  {deleteLoading ? (
+                    <ClipLoader size={20} color="white" />
+                  ) : (
+                    "Yes"
+                  )}
                 </button>
                 <button
-                  className="bg-red-500 text-black font-semibold px-6 py-1 rounded-lg hover:scale-105 hover:cursor-pointer"
+                  className="bg-red-500 text-white font-semibold px-6 py-1 rounded-lg hover:scale-105 hover:cursor-pointer"
                   onClick={() => {
-                    setOpenDelete(false);
+                    setOpenDeleteModal(false);
                   }}
                 >
                   No
                 </button>
               </div>
+            </div>
+          </div>
+        )}
+        {/* Add Modal */}
+        {openAddModal && (
+          <div className="fixed inset-0 bg-black/40 backdrop-blur-sm flex items-center justify-center z-50 w-full">
+            <div className="p-5 bg-purple-500 rounded w-[50%] relative">
+              <RiCloseLine
+                size={30}
+                onClick={() => setOpenAddModal(false)}
+                className="absolute top-4 right-4 text-white hover:cursor-pointer hover:scale-110"
+              />
+              <h1 className="text-white text-xl font-semibold text-center">
+                Add Task
+              </h1>
+              <Formik
+                initialValues={initialValues}
+                validationSchema={addSchema}
+                validateOnBlur
+                validateOnChange
+                onSubmit={handleAddSubmit}
+              >
+                {({ isSubmitting, isValid, touched, errors, status }) => (
+                  <Form noValidate className="mt-2 flex flex-col gap-y-4">
+                    <div>
+                      <label className={tailwind.addFormLabel}>Title</label>
+                      <Field
+                        name="title"
+                        placeholder="type title here..."
+                        type="text"
+                        className="w-full bg-white p-2 rounded"
+                      />
+                      <ErrorMessage
+                        className="text-red-600"
+                        name="title"
+                        component="div"
+                      />
+                    </div>
+                    <div>
+                      <label className={tailwind.addFormLabel}>
+                        Description
+                      </label>
+                      <Field
+                        name="description"
+                        placeholder="type description here..."
+                        as="textarea"
+                        className="w-full bg-white p-2 rounded h-32"
+                      />
+                      <ErrorMessage
+                        className="text-red-600"
+                        name="description"
+                        component="div"
+                      />
+                    </div>
+                    <div className="flex flex-col">
+                      <label className={tailwind.addFormLabel}>Due Date</label>
+                      <Field
+                        name="date"
+                        type="date"
+                        className="bg-white p-2 rounded hover:cursor-pointer"
+                      />
+                    </div>
+                    <div className="flex flex-col">
+                      <label className={tailwind.addFormLabel}>Status</label>
+                      <Field
+                        name="status"
+                        as="select"
+                        className="bg-white p-2 rounded hover:cursor-pointer"
+                      >
+                        <option value="">Select Status</option>
+                        <option value="completed">Completed</option>
+                        <option value="in-progress">In-progress</option>
+                      </Field>
+                    </div>
+                    <div className="flex flex-col">
+                      <label className={tailwind.addFormLabel}>Priority</label>
+                      <Field
+                        name="priority"
+                        as="select"
+                        className="bg-white p-2 rounded hover:cursor-pointer"
+                      >
+                        <option value="">Select Priority</option>
+                        <option value="low">low</option>
+                        <option value="medium">Medium</option>
+                        <option value="high">High</option>
+                      </Field>
+                    </div>
+                    <button
+                      type="submit"
+                      className="mt-6 flex flex-col items-center rounded bg-white text-purple-600 w-[30%] py-1 mx-auto font-semibold hover:cursor-pointer hover:scale-105 hover:text-white hover:bg-purple-600"
+                    >
+                      {isSubmitting ? (
+                        <div class="w-6 h-6 border-2  border-t-transparent rounded-full animate-spin"></div>
+                      ) : (
+                        "Submit"
+                      )}
+                    </button>
+                  </Form>
+                )}
+              </Formik>
             </div>
           </div>
         )}
@@ -147,4 +331,5 @@ const tailwind = {
   taskPriority: "priority font-semibold text-sm",
   buttons:
     "text-white hover:scale-110 hover:cursor-pointer transform transition duration-300",
+  addFormLabel: "font-semibold text-white",
 };
